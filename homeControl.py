8cfd05f97ea2fc4c297cd28ed68f597ab12b5cbf
root <root@raspberrypi.(none)>
2014-06-28 17:10:20 +0200

    Añadida configuracion de ficheros estaticos en nginx y añadida la plantilla principal

--- homeControl.py
+++ homeControl.py
@@ -1,13 +1,17 @@
 from flask import Flask
+from jinja2 import Environment, PackageLoader
 app = Flask(__name__)
-
+env = Environment(loader=PackageLoader('homeControl', 'html'))
+		
 @app.route('/')
 def homeControl():
-    return 'Hello World!'
+	template = env.get_template('index.html')
+	return template.render(the='variables', go='here')
+	
 	
 @app.route('/prueba/')
-def Prueba():
+def Prueba(): 
     return 'prueba'
 
 if __name__ == '__main__':
-    app.run()
+	app.run(debug=True)
