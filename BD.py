a53050b0c558385fbe8572c349465b57f50cd576
oottee <root@raspberrypi.(none)>
2014-07-03 18:28:21 +0200

    Añadidos nuevos archivos y bd de pruebas

---
+++ BD.py
@@ -0,0 +1,24 @@
+﻿import sqlite3
+from flask import g, Flask
+
+DATABASE = '/var/www/homeControl/db/dataBase.db'
+
+class conn:
+	def __init__(self):
+		self.db = getattr(g, '_database', None)
+		if self.db is None:
+			self.db = g._database = sqlite3.connect(DATABASE)
+	
+	def close(self):
+		self.db = getattr(g, '_database', None)
+		if self.db is not None:
+			self.db.close()
+	
+	def query(self,query, args=(), one=False):
+		cur = self.db.execute(query,args)
+		rv = cur.fetchall()
+		cur.close()
+		return (rv[0] if rv else None) if one else rv
+	
+	def commit(self):
+		self.db.commit()
--- homeControl.py
+++ homeControl.py
@@ -1,30 +1,62 @@
 from flask import Flask
 from jinja2 import Environment, PackageLoader
+from BD import conn
+
+
 app = Flask(__name__)
+app.config['DEBUG'] = True
 env = Environment(loader=PackageLoader('homeControl', 'html'))
 		
 @app.route('/')
 def homeControl():
+	c  = conn()
+	habitaciones = c.query('select * from habitacion')
+	
+	#print c.query('insert into habitacion (name) values (\'Salon\')')
+	#print c.query('insert into habitacion (name) values (\'Cocina\')')
+	#print c.query('insert into habitacion (name) values (\'Viky\')')
+	#c.commit()
+	c.close()
 	template = env.get_template('index.html')
-	return template.render(pagina='inicio.html')
+	values={ 'pagina' : 'inicio.html',
+			'habitaciones' : habitaciones
+	}
+	return template.render(values)
+	
+
 	
 	
 @app.route('/habitacion')
 def habitacion(): 
+	c  = conn()
+	habitaciones = c.query('select * from habitacion')
+	c.close()
 	template = env.get_template('index.html')
-	return template.render(pagina='habitacion.html')
+	values={ 'pagina' : 'habitacion.html',
+			'habitaciones' : habitaciones
+	}
+	return template.render(values)
 	
 @app.route('/adminHabitaciones')
 def adminHabitacion(): 
+	c  = conn()
+	habitaciones = c.query('select * from habitacion')
+	c.close()
 	template = env.get_template('index.html')
-	return template.render(pagina='adminHabitaciones.html')
+	values={ 'pagina' : 'adminHabitaciones.html',
+			'habitaciones' : habitaciones
+	}
+	return template.render(values)
 	
 @app.route('/adminComponentes')
 def adminComponentes(): 
+	c  = conn()
+	habitaciones = c.query('select * from habitacion')
+	c.close()
 	template = env.get_template('index.html')
-	return template.render(pagina='adminComponentes.html')
-
-	
+	values={ 'pagina' : 'adminComponentes.html',
+	}
+	return template.render(values)
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
